d0a5650cc5fb937bff8482b543b12dc1189a0bae
thecodingrunner <finn.moffett@gmail.com>
2025-01-14 16:34:54 +0000

    Create Album Class

---
+++ RecordApplication/Entities/Album.cs
@@ -0,0 +1,25 @@
+﻿namespace RecordApplication.Entities
+{
+    public class Album
+    {
+        public Album(string albumName, int artistId, Artist artist, int releaseYear, int units, Genre genre, string description)
+        {
+            AlbumName = albumName;
+            ArtistId = artistId;
+            Artist = artist;
+            ReleaseYear = releaseYear;
+            Units = units;
+            Genre = genre;
+            Description = description;
+        }
+
+        public int Id { get; set; }
+        public string AlbumName { get; set; }
+        public int ArtistId { get; set; }
+        public Artist Artist { get; set; }
+        public int ReleaseYear { get; set; }
+        public int Units { get; set; }
+        public Genre Genre { get; set; }
+        public string Description { get; set; }
+    }
+}
