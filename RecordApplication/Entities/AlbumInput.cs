8268652cf7a831319f9b2cc58ad35151428b652a
thecodingrunner <finn.moffett@gmail.com>
2025-01-14 16:35:17 +0000

    Create AlbumInput Class

---
+++ RecordApplication/Entities/AlbumInput.cs
@@ -0,0 +1,23 @@
+﻿namespace RecordApplication.Entities
+{
+    public class AlbumInput
+    {
+        public AlbumInput(string albumName, string artistName, int releaseYear, int units, Genre genre, string description)
+        {
+            AlbumName = albumName;
+            ArtistName = artistName;
+            ReleaseYear = releaseYear;
+            Units = units;
+            Genre = genre;
+            Description = description;
+        }
+
+        public int Id { get; set; }
+        public string AlbumName { get; set; }
+        public string ArtistName { get; set; }
+        public int ReleaseYear { get; set; }
+        public int Units { get; set; }
+        public Genre Genre { get; set; }
+        public string Description { get; set; }
+    }
+}
