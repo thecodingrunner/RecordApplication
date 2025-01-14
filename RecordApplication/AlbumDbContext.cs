46e45b44c2ef6de6d7fd0b5e291f28a5bce47087
thecodingrunner <finn.moffett@gmail.com>
2025-01-14 16:36:34 +0000

    Setup Program file and Database Context

---
+++ RecordApplication/AlbumDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RecordApplication.Entities;
+
+namespace RecordApplication
+{
+    public class AlbumDbContext : DbContext
+    {
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+
+        public AlbumDbContext(DbContextOptions<AlbumDbContext> options) : base(options)
+        {
+        }
+    }
+}
--- RecordApplication/Program.cs
+++ RecordApplication/Program.cs
@@ -1,4 +1,8 @@
 
+using Microsoft.EntityFrameworkCore;
+using RecordApplication.Models;
+using RecordApplication.Services;
+
 namespace RecordApplication
 {
     public class Program
@@ -7,9 +11,24 @@ namespace RecordApplication
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            //string connectionString = builder.Configuration.GetConnectionString("DevelopmentConnectionString");
+
             // Add services to the container.
 
             builder.Services.AddControllers();
+
+            builder.Services.AddTransient<IAlbumsService, AlbumsService>();
+            builder.Services.AddTransient<IAlbumsModel, AlbumsModel>();
+
+            if (builder.Environment.IsDevelopment())
+            {
+                builder.Services.AddDbContext<AlbumDbContext>(options => options.UseInMemoryDatabase("InMemoryDb"));
+            }
+            else if (builder.Environment.IsProduction()) 
+            {
+                builder.Services.AddDbContext<AlbumDbContext>(options => options.UseSqlServer("DefaultConnection"));
+            }
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
