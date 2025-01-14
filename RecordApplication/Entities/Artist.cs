9878e151a5837a38c8eb0bf8f6b210fd7719480a
thecodingrunner <finn.moffett@gmail.com>
2025-01-14 16:35:28 +0000

    Create Artist Class

---
+++ RecordApplication/Entities/Artist.cs
@@ -0,0 +1,13 @@
+﻿namespace RecordApplication.Entities
+{
+    public class Artist
+    {
+        public Artist(string name)
+        {
+            Name = name;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
